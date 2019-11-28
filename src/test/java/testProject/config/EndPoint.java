db48e0652f2f4c9c5c2fa5126c58ca7fe1f2105d
AM <anvar.minulin@gmail.com>
2019-11-28 21:57:24 +0300

    Add test to check json response body

--- src/test/java/testProject/config/EndPoint.java
+++ src/test/java/testProject/config/EndPoint.java
@@ -2,4 +2,5 @@ package testProject.config;
 
 public interface EndPoint {
     String JSONPLACEHOLDER_POSTS = "/posts";
+    String JSONPLACEHOLDER_USERS = "/users";
 }
--- src/test/java/testProject/suits/JsonPlaceHolderTestSuite.java
+++ src/test/java/testProject/suits/JsonPlaceHolderTestSuite.java
@@ -1,10 +1,12 @@
 package testProject.suits;
 
 import io.qameta.allure.Step;
+import io.restassured.response.Response;
 import org.junit.Test;
 import io.restassured.RestAssured;
 import testProject.config.EndPoint;
 import testProject.config.TestsConfig;
+import static org.hamcrest.Matchers.*;
 
 
 import static io.restassured.RestAssured.*;
@@ -15,6 +17,13 @@ public class JsonPlaceHolderTestSuite extends TestsConfig {
     @Step("Send GET request to /posts, got list of posts and response code = 200")
     public void positiveGetAllPosts(){
         get(EndPoint.JSONPLACEHOLDER_POSTS);
+    }
+
+    @Test
+    @Step("Username 'Bret' in response body")
+    public void positiveCheckUserName(){
+        Response response = get(EndPoint.JSONPLACEHOLDER_USERS);
+        response.then().body("username", hasItem("Bret"));
     }
 
 }
